ae78b1defc650f39ccdba92d913b88b6f3319219
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-04-24 17:00:55 -0400

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,7 +1,7 @@
 const { createYield } = require("typescript")
 
 describe('First test', () => {
-    it('Open webapp', () => {
+    it('Open webapp page', () => {
         cy.visit('')
         cy.get('.card').eq(1).click()
     })
